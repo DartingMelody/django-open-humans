ea3d64484053332e547316ab499ea3875b612d51
DartingMelody <tkhaniitr@gmail.com>
2018-08-15 14:05:33 +0530

    resolved conflicts

--- openhumans/views.py
+++ openhumans/views.py
@@ -15,44 +15,6 @@ import ohapi
 import requests
 
 from .helpers import oh_code_to_member, oh_client_info
-<<<<<<< HEAD
-OH_BASE_URL = settings.OPENHUMANS_OH_BASE_URL
-OH_API_BASE = OH_BASE_URL + '/api/direct-sharing'
-
-
-def login_member(request):
-    code = request.GET.get('code', '')
-    try:
-        oh_member = oh_code_to_member(code=code)
-    except Exception:
-        oh_member = None
-        print("exception while logging in")
-    if oh_member:
-        # Log in the user.
-        user = oh_member.user
-        login(request, user,
-              backend='django.contrib.auth.backends.ModelBackend')
-
-
-def complete(request):
-    """
-    Receive user from Open Humans. Store data, start data upload task.
-    """
-    # logger.debug("Received user returning from Open Humans.")
-
-    login_member(request)
-    if not request.user.is_authenticated:
-        # logger.debug('Invalid code exchange. User returned to start page.')
-        print('Invalid code exchange. User returned to start page.')
-        return redirect('/')
-    else:
-        return redirect('overview')
-"""
-Create your views here.
-"""
-=======
-
->>>>>>> trying
 from django.views import View
 from django.shortcuts import render, redirect
 
